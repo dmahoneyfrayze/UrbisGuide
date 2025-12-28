0ffbbdc7a662e8051769d08b5f00dc481afdbe82
Denis Mahoney <denismahoney@Mac-mini.local>
2025-12-27 23:52:09 -0500

    Perf: Implement Partytown, LCP Optimization, Mobile Menu Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import sitemap from '@astrojs/sitemap';
 
 import mdx from '@astrojs/mdx';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -18,5 +20,13 @@ export default defineConfig({
     format: 'directory'
   },
 
-  integrations: [sitemap(), mdx()]
+  integrations: [
+    sitemap(),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "fbq"],
+      },
+    })
+  ]
 });
